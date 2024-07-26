53545abefcf6adec2ae0c66314e7d0065b619b6b
darya123-git <daryarckina@yandex.ru>
2024-07-27 00:51:31 +0300

    Initial commit

---
+++ Zadanie_2/Zadanie_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите неотрицательное число m: "); 
+            int m=int.Parse(Console.ReadLine()); 
+            Console.Write("Введите неотрицательное число n: "); 
+            int n=int.Parse(Console.ReadLine()); 
+            int result=Ackermann(m, n); 
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        static int Ackermann(int m, int n)
+        { 
+            if (m==0) 
+                return n+1; 
+            else if (n==0) 
+                return Ackermann(m-1, 1); 
+            else 
+                return Ackermann(m-1, Ackermann(m, n-1)); 
+        } 
+    }
+}
---
+++ Zadanie_3/Zadanie_3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]A={5,6,9,8,7,3};
+            Console.WriteLine("Элементы массива, начиная с конца:");
+            PrintArray(A, A.Length-1);
+            Console.ReadLine();
+        }
+
+        static void PrintArray(int[] B, int i)
+        {
+            if (i>=0)
+            {
+                Console.Write(B[i]+" ");
+                PrintArray(B,i-1);
+            }
+        }
+
+    }
+}
